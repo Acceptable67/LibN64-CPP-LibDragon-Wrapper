660976aafa17a3f2a88a38128437978f9aab61b4
Acceptable67 <spencerchase45@hotmail.com>
2021-12-17 19:26:17 -0500

    infinite arguments with va_list and svprintf
    Signed-off-by: Acceptable67 <spencerchase45@hotmail.com>

--- LibN64GameTest/LibN64.h
+++ LibN64GameTest/LibN64.h
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #define RED				graphics_make_color(0xFF, 0x00, 0x00, 0xFF)
 #define GREEN			graphics_make_color(0x00, 0xFF, 0x00, 0xFF)
@@ -24,103 +25,85 @@
 
 
 namespace LibN64 {
-
-namespace DFS {
-
-	unsigned Open(const char* file) ;
-	int      Size();
-	void	 Read(char* buf, unsigned size, unsigned offset = 0x0) ;
-	char*    QuickRead(const char* file);
-	void     Close();
-};
+	namespace DFS 
+	{
+		unsigned Open(const char* file) ;
+		int      Size(const char* file);
+		void	 Read(char* buf, unsigned size, unsigned offset = 0x0) ;
+		char*    QuickRead(const char* file);
+		void     Close();
+	};
 
 class Frame {
+	private:
+		virtual void FrameUpdate();
+		virtual void KeyAPressed();
+		virtual void KeyBPressed();
+		virtual void KeyZPressed();
 
-private:
-	virtual void FrameUpdate();
-	virtual void KeyAPressed();
-	virtual void KeyBPressed();
-	virtual void KeyZPressed();
+		virtual void KeyStartPressed();
 
-	virtual void KeyStartPressed();
+		virtual void KeyDUPressed();
+		virtual void KeyDDPressed();
+		virtual void KeyDLPressed();
+		virtual void KeyDRPressed();
+		
+		virtual void KeyJoyXPressed();
+		virtual void KeyJoyYPressed();
 
-	virtual void KeyDUPressed();
-	virtual void KeyDDPressed();
-	virtual void KeyDLPressed();
-	virtual void KeyDRPressed();
+		unsigned screenWidth;
+		unsigned screenHeight;
 
-	unsigned screenWidth;
-	unsigned screenHeight;
+		void CheckAndSwitchRes(resolution_t r);
+	public:
+		display_context_t d;
 
-	void CheckAndSwitchRes(resolution_t r);
-public:
-	display_context_t d;
+		const char* romTitle = "Default";
+		bool lActive;
 
-	const char* romTitle = "Default";
-	bool lActive;
+		float fFrameTime;
+		float fTotalTime;
+	public:
+		Frame(resolution_t res, bitdepth_t dep);
+		virtual void OnCreate();
+		void 	 Begin();
+		void 	 Close();
+		void 	 ClearScreen();
+		void 	 SetScreen(resolution_t res, bitdepth_t bd);
+		unsigned ScreenWidth();
+		unsigned ScreenHeight();
+		void 	 DrawText(int x, int y, const char* t,    unsigned c = WHITE);
+		void 	 DrawPixel(int x, int y, 		      	  unsigned c = WHITE);
+		void 	 DrawBox(int x, int y, int scale = 1,     unsigned c = WHITE);
+		void 	 DrawLine(int x1, int y1, int x2, int y2, unsigned c = WHITE);
+		void  	 DrawCircle(int x, int y, int scale = 1,  unsigned c = WHITE);
+		float Ticks2Seconds(float t);
 
-	float fFrameTime;
-	float fTotalTime;
-public:
-	Frame(resolution_t res, bitdepth_t dep);
-	virtual void OnCreate();
-	void Begin();
-	void Close();
-	void ClearScreen();
-	void SetScreen(resolution_t res, bitdepth_t bd);
-	unsigned ScreenWidth();
-	unsigned ScreenHeight();
-	void DrawText(int x, int y, const char* t,    unsigned c = WHITE);
-	void DrawPixel(int x, int y, 		      unsigned c = WHITE);
-	void DrawBox(int x, int y, int scale = 1,     unsigned c = WHITE);
-	void DrawLine(int x1, int y1, int x2, int y2, unsigned c = WHITE);
-	void DrawCircle(int x, int y, int scale = 1,  unsigned c = WHITE);
-	float Ticks2Seconds(float t);
-
-	/*The following functions refuse to compile inside the C++ file.*/
-	/*DFS does not work so here is work around. Manually find*/
-public:
-	template<class T>
-	T* __lib64_rom2buf(long romAddr, int size) {
-		T* tmp = (T*)malloc(size + sizeof(T));
-		for (auto i = 0; i < size; i++) {
-			tmp[i] = __lib64_rom2type<T>(romAddr + (i*sizeof(T)));
+		/*The following functions refuse to compile inside the C++ file.*/
+		/*DFS does not work so here is work around. Manually find*/
+	public:
+		template<class T>
+		T* __lib64_rom2buf(long romAddr, int size) {
+			T* tmp = (T*)malloc(size + sizeof(T));
+			for (auto i = 0; i < size; i++) {
+				tmp[i] = __lib64_rom2type<T>(romAddr + (i*sizeof(T)));
+			}
+			return tmp;
 		}
-		return tmp;
-	}
 
-	template<class T>
-	T __lib64_rom2type(long romAddr) {
-		T *ptr = (T*)(romAddr);
-		return *ptr;
-	}
+		template<class T>
+		T __lib64_rom2type(long romAddr) {
+			T *ptr = (T*)(romAddr);
+			return *ptr;
+		}
 
-	template<class Arg1>
-	constexpr void DrawTextFormat(int x, int y, const char* buf, Arg1 arg1) {
-		char buffer[strlen(buf)];
-		sprintf(buffer, buf, arg1);
-		graphics_draw_text(d, x, y, buffer);
-	}
-
-	template<class Arg1, class Arg2>
-	constexpr void DrawTextFormat(int x, int y, const char* buf, Arg1 arg1, Arg2 arg2) {
-		char buffer[strlen(buf)];
-		sprintf(buffer, buf, arg1, arg2);
-		graphics_draw_text(d,x, y, buffer);
-	}
-
-	template<class Arg1, class Arg2, class Arg3>
-	constexpr void DrawTextFormat(int x, int y, const char* buf, Arg1 arg1, Arg2 arg2, Arg3 arg3) {
-		char buffer[strlen(buf)];
-		sprintf(buffer, buf, arg1, arg2, arg3);
-		DrawText(x, y, buffer);
-	}
-
-	template<class Arg1, class Arg2, class Arg3, class Arg4>
-	constexpr void DrawTextFormat(int x, int y, const char* buf, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) {
-		char buffer[strlen(buf)];
-		sprintf(buffer, buf, arg1, arg2, arg3, arg4);
-		DrawText(x, y, buffer);
-	}
-};
+		void DrawTextFormat(int x, int y, const char* format, ...) {
+		   va_list args;
+		   va_start(args, format);
+		   char buffer[85];
+		   vsprintf(buffer, format, args);
+		   graphics_draw_text(d, x, y,buffer);
+		   va_end(args);	
+		}
+	};
 }
